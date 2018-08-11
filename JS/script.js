ad835df5ac14096ef530851768ccd0c894efc37f
Leprince Kelly <kellyleprince.simplon@gmail.com>
2018-08-11 23:15:44 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+    var personne = prompt("Entrez votre prénom");
+    
+    if (personne != null) {
+    document.getElementById("result").innerHTML =
+    "Bonjour " + personne + "! Comment vas-tu aujoud'hui?";        
+    console.log("Bonjour " + personne + "! Comment vas-tu aujoud'hui?")
+    }
